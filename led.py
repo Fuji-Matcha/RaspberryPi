ea989dd1a56eb62e87c72f7f2760938bc6d47dcb
Fuji_Matcha <00002191hiroki@gmail.com>
2020-03-26 15:52:32 +0900

    LEDの点灯と消灯

---
+++ led.py
@@ -0,0 +1,16 @@
+import wiringpi as pi
+
+def main():
+
+    LED_PIN = 8
+    pi.wiringPiSetupGpio()
+    pi.pinMode( LED_PIN, pi.OUTPUT )
+    pi.digitalWrite( LED_PIN, pi.HIGH)
+    pi.delay(5000)
+    pi.digitalWrite( LED_PIN, pi.LOW)
+
+if __name__ == "__main__" :
+
+    main()
+
+
